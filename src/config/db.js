1f83b30e840438db05da69f0aa912ab6eaaa9f31
QUANG-SanBay <quangsanbay@gmail.com>
2026-07-14 19:07:05 +0700

    chore: setup eslint, change module type, and add formatting tools

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,8 @@ const connectDB = async () => {
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Database connection error: ${error.message}`);
-        process.exit(1);
+        // process.exit(1);
+        throw error;
     }
 };
 
